fa5ee5bd0381b936dcdc70fe57888ac35b82fcae
lesg2605 <lesg2605@usherbrooke.ca>
2022-06-21 16:39:23 -0400

    Added client file

---
+++ client.cpp
@@ -0,0 +1,11 @@
+#include <rdma/rsocket.h>
+#include <rdma/rdma_cma.h>
+#include <iostream>
+#include <arpa/inet.h>
+
+constexpr unsigned int DEST_PORT = 6969;
+constexpr unsigned int SOURCE_PORT = 4242;
+
+int main(int argc, char **argv) {
+
+}
