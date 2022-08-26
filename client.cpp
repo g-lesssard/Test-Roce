5fc3935541515346cd9c0091f8e018dcd51e8897
Gabriel Lessard <lesg2605@usherbrooke.ca>
2022-08-26 13:56:33 -0400

    Better logs for send and receive client

--- client.cpp
+++ client.cpp
@@ -9,24 +9,24 @@
 
 constexpr unsigned int DEST_PORT = 6969;
 constexpr unsigned int SOURCE_PORT = 4242;
-constexpr const char DEST_ADDR[] = "10.0.0.69";
+constexpr const char DEST_ADDR[] = "10.42.0.1";
 
 int main(int argc, char **argv) {
     int socket = rsocket(AF_INET, SOCK_STREAM, 0);
     if (socket < 0) {
-        std::cerr << "Failed to create client socket" << std::endl;
+        std::cerr << "[ERROR] Failed to create client socket" << std::endl;
         return -1;
     }
-    std::cout << "Created client socket" << std::endl;
+    std::cout << "[INFO] Created client socket" << std::endl;
 
     sockaddr_in server_addr;
     hostent *server;
     server = gethostbyname(DEST_ADDR);
     if (!server) {
-        std::cerr << "No such destination!\n";
+        std::cerr << "[ERROR] No such destination!\n";
         return -3;
     }
-    std::cout << "Destination is found.\n";
+    std::cout << "[INFO] Destination is found.\n";
 
     std::memset((char*)&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
@@ -34,16 +34,16 @@ int main(int argc, char **argv) {
     inet_pton(AF_INET, DEST_ADDR, &server_addr.sin_addr);
 
     if (rconnect(socket, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Connection could not be established!\n";
+        std::cerr << "[ERROR] Connection could not be established!\n";
         return -6;
     }
     std::cout << "[INFO] Connection established.\n";
     char message[] = "Hello RDMA!";
     if (rsend(socket, message, sizeof(message), 0) < 0)
         std::cerr << "[ERROR] Failed to send message";
-    std::cout << "Program teardown..." << std::endl;
+    std::cout << "[INFO] Program teardown..." << std::endl;
     rclose(socket);
-    std::cout << "DONE!" << std::endl;
+    std::cout << " [INFO] DONE!" << std::endl;
 
 
 }
